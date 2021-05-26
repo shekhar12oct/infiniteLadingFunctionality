e8196d21307200a956a817bfc6be2b4ce389e9e7
shekhar125 <44122666+shekhar125@users.noreply.github.com>
2021-05-27 00:45:52 +0530

    infinte loading functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import {React,useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+// Initial upto which number the result should be seen to us.
+const start_Number=0;
 
 function App() {
+  const [state,setState]=useState([]);
+  const [number,setNumber]=useState(start_Number);
+
+  
+// creatng a hook to fetch the api usin axios
+  useEffect(() => {
+    axios.get(`http://jsonplaceholder.typicode.com/posts?_start=${number}&_limit=10`)
+    .then(res=>{
+      setState([...state,...res.data])
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  },[number]);
+
+  //Adding the scrolling function to update the number
+  const scrollToEnd=()=>{
+    setNumber(number+10);
+  };
+// adding scroll browser api call
+  window.onscroll=function(){
+    // scrollToEnd();
+    //checked if the page has scrolled to the bottom
+    if(
+      window.innerHeight + document.documentElement.scrollTop+1 >= 
+        document.scrollingElement.scrollHeight
+    ){
+      scrollToEnd();
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {state.map((ele,i)=>(
+        <div key={i} className="container">
+        <h4>UserID:{ele.userId}</h4>
+        <h4>Id:{ele.id}</h4>
+        <h4>Title:{ele.title}</h4>
+        <h4>Body :{ele.body}</h4>
+        </div>
+    )
+      )
+      }
     </div>
   );
 }
+
 
 export default App;
